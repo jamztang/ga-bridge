ecaa5f1da4f1143d8429a347f665a9101d272a48
Jamz Tang <jamz@jamztang.com>
2013-09-27 17:36:32 +0800

    Initial Commit

---
+++ ga-bridge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ga-bridge
+//
+//  Created by Jamz Tang on 27/9/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ga-bridge/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ga-bridge
+//
+//  Created by Jamz Tang on 27/9/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
